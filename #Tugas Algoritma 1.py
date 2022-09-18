3b9ded538b7200e26838c8e093adc419ce6e34a2
Muhammad Ali Rhojab <113070562+muhammadali934@users.noreply.github.com>
2022-09-18 10:34:34 +0700

    Add files via upload

--- #Tugas Algoritma 1.py
+++ #Tugas Algoritma 1.py
@@ -1,9 +1,9 @@
 #Tugas Algoritma 1
 
 salam = input("Greeting ?")
-if salam.startswith ("h") :
+if salam == ("hello") :
+    print("$0")
+elif salam.startswith ("h") :
     print ("$20")
-elif salam == ("hello") :
-    print ("$0")
 else :
     print ("$100")
